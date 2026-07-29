22673f09403d42a653f44f7c6b15f4ca8bfc135b
kunal <150831705+keneynicxkunal@users.noreply.github.com>
2026-07-29 19:31:04 +0530

    Time: 0 ms (100%), Space: 8.8 MB (57.48%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> nums;
+        vector<int> fact(n);
+
+        fact[0] = 1;
+        for (int i = 1; i < n; i++)
+            fact[i] = fact[i - 1] * i;
+
+        for (int i = 1; i <= n; i++)
+            nums.push_back(i);
+
+        k--; // Convert to 0-based indexing
+
+        string ans = "";
+
+        for (int i = n; i >= 1; i--) {
+            int index = k / fact[i - 1];
+            ans += to_string(nums[index]);
+
+            nums.erase(nums.begin() + index);
+
+            k %= fact[i - 1];
+        }
+
+        return ans;
+    }
+};
