65b487ff754436896859529a9f1019af171a5a74
kunal <150831705+keneynicxkunal@users.noreply.github.com>
2026-08-25 14:50:53 +0530

    Time: 0 ms (100%), Space: 25.1 MB (45.06%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        int multiple = k;
+
+        while (st.count(multiple)) {
+            multiple += k;
+        }
+
+        return multiple;
+    }
+};
